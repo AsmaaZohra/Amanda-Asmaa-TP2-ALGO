ca68eb40298cee6cdcee9f3a2071c151040d7ae1
amanda-008 <amanda.dorval@umontreal.ca>
2025-04-20 10:50:07 -0400

    Update MinCostRechargeCalculator.h

--- code 3/bornes_recharges/MinCostRechargeCalculator.h
+++ code 3/bornes_recharges/MinCostRechargeCalculator.h
@@ -1,14 +1,56 @@
+#include "MinCostRechargeCalculator.h"
 #include <vector>
+#include <math.h>
+#include <iostream>
+#include <algorithm> // for std::min
 
-// Nom(s) étudiant(s) / Name(s) of student(s):
+// Nom(s) étudiant(s) / Name(s) of student(s): Amanda Dorval et Asmaa Skou
 
-// ce fichier contient les declarations des methodes de la classe MaxToysCalculator
-// peut être modifié si vous voulez ajouter d'autres méthodes à la classe
-// this file contains the declarations of the methods of the MaxToysCalculator class
-// can be modified if you wish to add other methods to the class
+// ce fichier contient les definitions des methodes de la classe MinCostRechargeCalculator
+// this file contains the definitions of the methods of the MinCostRechargeCalculator class
 
-class MinCostRechargeCalculator{
-    public :
-    MinCostRechargeCalculator();
-        int CalculateMinCostRecharge(const std::vector<int>& arr);
-};
+using namespace std;
+
+MinCostRechargeCalculator::MinCostRechargeCalculator() {
+}
+
+int MinCostRechargeCalculator::CalculateMinCostRecharge(const vector<int>& RechargeCost) {
+    int n = RechargeCost.size(); // nombre de bornes par lequel le camion va passer
+    vector<int> tabProgDyn(n + 2, INT_MAX); // tabProgDyn[i] qui prend le cout min à la position i
+
+    // cas de base (départ du camion quand n=0 et coût borne=0)
+    tabProgDyn[0] = 0;
+
+    // cas de récurrence (parcourir les bornes 1 à n et trajet du camion (n+1))
+    for (int i = 1; i <= n + 1; i++) {
+
+        // on vérifie les 3 positions avant soit j = i-1, j = 1-2, j = i-3
+        for (int j = 1; j <= 3 && i - j >= 0; j++) {
+
+            // si tabProgDyn[i-j] on peut y accéder alors pas besoin de la valeur maximale
+            if (tabProgDyn[i - j] != INT_MAX) {
+
+                // si i est le trajet du camion soit i == n+1 donc pas de recharge à faire
+                if (i == n + 1) {
+                    tabProgDyn[i] = min(tabProgDyn[i], tabProgDyn[i - j]);
+                } else {
+
+                    // sinon si i est pas le trajet on va ajouter le cout de la borne i-1
+                    tabProgDyn[i] = min(tabProgDyn[i], tabProgDyn[i - j] + RechargeCost[i - 1]);
+                }
+            }
+        }
+    }
+
+    // retourne le coût minimal pour faire le trajet complet
+    return tabProgDyn[n + 1];
+}
+
+
+// À ENLEVER!!!
+// SI TU VEUX LE TESTER SUR MACBOOK VSCODE ÉCRIT CA DANS TON TERMINAL A LA PLACE: g++ -std=c++11 -o min_cost_recharge min_cost_recharge.cpp MinCostRechargeCalculator.cpp
+// ./min_cost_recharge
+
+
+
+
