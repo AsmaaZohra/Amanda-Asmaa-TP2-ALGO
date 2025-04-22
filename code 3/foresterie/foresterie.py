b55251433b38c317168be1b08c334cfb064ae374
Asmaa Skou <91071080+AsmaaZohra@users.noreply.github.com>
2025-04-21 22:08:29 -0400

    forest
    did the solve function

--- code 3/foresterie/foresterie.py
+++ code 3/foresterie/foresterie.py
@@ -1,21 +1,38 @@
-  
-# Nom(s) étudiant(s) / Name(s) of student(s):
+ # Name(s) of student(s): Asmaa Zohra Skou et Amanda Dorval
 
 import sys
 
-# Espace pour fonctions auxillaires :
-# Space for auxilary functions :
-
-
 
 # Fonction à compléter / function to complete:
+
+# The problem is similar to the house robber problem 
+# Considering the test file, we only need to give the profits as the answer and not the trees used to get that output 
 def solve(cost, forest) :
-    return 
+    profits = []
+    for val in forest:
+        profits.append(val - cost)
+
+    n = len(profits) 
+
+    if n == 0:
+       return 0
+    if n == 1:
+       return max (0,profits[0]) #do not take in consideration a tree with a negative cost  
+     
+    tree_n_2 = 0 #best profit up to tree i-2
+    tree_n_1 = max(0,profits[0]) #best profit up to tree i-1
+
+    for i in range(1, n):
+        current = max(tree_n_1, tree_n_2 + profits[i]) #you can either skip the current tree(profit tree n-1) or cut it (add to tree n-2) 
+        #depending on the profit 
+        tree_n_2 = tree_n_1
+        tree_n_1 = current
+
+    return tree_n_1
 
 
-# Ne pas modifier le code ci-dessous :
+
 # Do not modify the code below :
-
 def process_numbers(input_file):
     try:
         # Read integers from the input file
@@ -41,4 +58,4 @@ def main():
     print(f"Result: {res}")
 
 if __name__ == "__main__":
-    main()
+    main() 
